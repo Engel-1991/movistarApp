794d7325b4098f9e1d0a5ed418662502865c8ee1
Engel-1991 <108898290+Engel-1991@users.noreply.github.com>
2023-04-11 20:37:21 -0500

    se agrega validacion para el acceso con el token

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { DataService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -16,13 +17,63 @@ export class LoginComponent implements OnInit {
     Validators.email,
   ]);
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private dataService: DataService) {}
   
   ngOnInit() {}
 
   submitToken() {
-    localStorage.setItem('token', this.token);
-    this.router.navigate(['home']);
+    var inventario = [
+      {
+        Serie: '',
+        Subserie: '',
+        NumIdentificacion: '',
+        NumDocumento: '',
+        NombreUnidad: '',
+        FechaDocumento: '',
+        LineaCelular: "3022930255",
+        PeticionNoOrden: '',
+        ReqObra: '',
+        IdInventario: '',
+        UserNCA: {
+          UsuarioRed: 'ghernandele',
+          CentroCosto: 'NA',
+          IdGerencia: 'NA',
+          IdJefatura: null,
+          Seccion: 'NA',
+          SubFondo: 'NA',
+          SubSeccion: 'NA',
+          Correo: 'NA',
+          NombreJefe: 'NA',
+          ApellidoJefe: 'NA',
+          CorreoJefe: 'NA',
+          CargoEmpleado: 'NA',
+        },
+        filtro: '',
+        pagina: 0,
+        tamano_pagina: 10,
+        Columna: '1',
+        Orden: 'asc',
+        Operacion: '1',
+        Estado: 1,
+      },
+    ];
+
+    const data = {
+      // Aquí colocarías los datos que quieres enviar
+      userList: inventario,
+      // ...
+    };
+
+    this.dataService.sendData(data.userList[0]).subscribe(
+      (response) => {
+        localStorage.setItem('token', this.token);
+        this.router.navigate(['home']);
+      },
+      (error) => {
+        alert('No fue posible iniciar sesión');
+        console.error(error);
+      }
+    );
   }
 }
 
